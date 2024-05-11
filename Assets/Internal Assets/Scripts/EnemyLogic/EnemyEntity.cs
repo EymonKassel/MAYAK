3762829344f6694790cf2d1f9137757c478c1ded
Eymon Kassel <55111120+EymonKassel@users.noreply.github.com>
2024-05-11 21:39:23 +0300

    yet another hotifx

--- Assets/Internal Assets/Scripts/EnemyLogic/EnemyEntity.cs
+++ Assets/Internal Assets/Scripts/EnemyLogic/EnemyEntity.cs
@@ -1,30 +1,27 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class EnemyEntity : MonoBehaviour {
-//    public Transform Player;
-//    public GameObject EnemyBullet;
+public class EnemyEntity : MonoBehaviour {
+    [SerializeField] private int _maxHealth = 3;
+    [SerializeField] private int _currentHealth;
 
-//    [SerializeField] private int _maxHealth = 3;
-//    [SerializeField] private int _currentHealth;
-
-//    private void Awake() {
-//        _currentHealth = _maxHealth;
-//    }
-//    private void Update() {
-//        if ( _currentHealth <= 0 ) Kill();
-//    }
-//    public void Kill() {
-//        Destroy(gameObject);
-//    }
-//    public void Hit() {
-//        _currentHealth--;
-//    }
-//    private void OnTriggerEnter2D(Collider2D collision) {
-//        if ( collision.gameObject.tag == "Trace" ) {
-//            Debug.Log("Hit");
-//            Hit();
-//        }
-//    }
-//}
+    private void Awake() {
+        _currentHealth = _maxHealth;
+    }
+    private void Update() {
+        if ( _currentHealth <= 0 ) Kill();
+    }
+    public void Kill() {
+        Destroy(gameObject);
+    }
+    public void Hit() {
+        _currentHealth--;
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if ( collision.gameObject.tag == "Trace" ) {
+            Debug.Log("Hit");
+            Hit();
+        }
+    }
+}
--- Assets/Internal Assets/Scripts/EnemyLogic/EnemyFollower.cs
+++ Assets/Internal Assets/Scripts/EnemyLogic/EnemyFollower.cs
@@ -1,56 +1,37 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class EnemyFollower : EnemyEntity {
-//    [SerializeField][Range(0, 100)] private float _pursuitSpeed = 1.0f;
+public class EnemyFollower : EnemyEntity {
+    [SerializeField] private Transform Player;
+    [SerializeField] private GameObject EnemyBullet;
 
-//    [Header("AI logic settings")]
-//    private bool _isFollowing = false;
-//    [SerializeField] private bool _isGlitched = false;
-//    [SerializeField] private bool _isShooter = false;
-//    [SerializeField] private bool _isCareful = false;
+    [SerializeField][Range(0, 100)] private float _pursuitSpeed = 1.0f;
 
-//    [Header("Shooter")]
-//    [SerializeField] private float _fireRate = 1.0f;
-//    [SerializeField] private float _bulletSpeed = 1.0f;
+    [Header("AI logic settings")]
+    [SerializeField] private bool _isFollowing = false;
+    [SerializeField] private bool _isDetonating = false;
+    [SerializeField] private bool _isShooting = false;
+    [SerializeField] private bool _isDashing = false;
 
+    private void Awake() {
+        _isFollowing = true;
+    }
 
-//    private void Awake() {
-//        //_isFollowing = true;
-//    }
+    private void Update() {
+        if ( _isFollowing ) Follow();
 
-//    private void Update() {
-//        //if (_isFollowing) Follow();
-//        //BeCareful();
+    }
+    private void Follow() {
+        transform.position = Vector3.MoveTowards(transform.position, Player.position, _pursuitSpeed * Time.deltaTime);
+    }
+    private IEnumerator Detonate() {
+        yield return null;
+    }
+    private void Shoot() {
+        Instantiate(EnemyBullet);
+    }
+    private void Dash() {
 
-
-//        // This can be replaced with a state machine or something like that, but I haven’t figured out how
-//        if ( _isGlitched ) BeGlitched();
-//        if ( _isShooter ) BeShooter();
-//        if ( _isCareful ) BeCareful();
-//    }
-//    private void Follow() {
-//        transform.position = Vector3.MoveTowards(transform.position, Player.position, _pursuitSpeed * Time.deltaTime);
-//    }
-//    private void BeCareful() {
-//        int i = 0;
-
-
-//        bool isCompletedCircle = false;
-//        while ( !isCompletedCircle && i != 1000 ) {
-//            transform.position = Vector2.Lerp(Vector2.zero, Vector2.one, 5f);
-
-
-
-
-//            i++;
-//        }
-//    }
-//    private void BeShooter() {
-
-//    }
-//    private void BeGlitched() {
-
-//    }
-//}
+    }
+}
